450c9a59bae0b3fd00397e07dae60d069b2f5341
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-06-23 12:28:03 -0500

    adds laura exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -26,17 +26,49 @@ public class ConsoleExercises {
 //
 //        System.out.printf("Your sentence is: %s", mySentence);
 
-        System.out.println("Please enter the length, width and height.");
+//        System.out.println("Please enter the length, width and height.");
+//
+//
+//        int length = Integer.parseInt(sc.next());
+//        int width = Integer.parseInt(sc.next());
+//        int height = Integer.parseInt(sc.next());
+//
+//        System.out.printf("Area: %s", length * width);
+//        System.out.printf(" Perimeter: %s", length * 2 + width * 2);
+//        System.out.printf(" Volume is : %s", length * width * height);
+//
+
+        System.out.println("Please enter a name");
+
+        String user1 = "Christian";
+        String user2 = "Marina";
+        String user3 = "Miyavi";
+
+        String user4 = sc.next();
 
 
-        int length = Integer.parseInt(sc.next());
-        int width = Integer.parseInt(sc.next());
-        int height = Integer.parseInt(sc.next());
 
-        System.out.printf("Area: %s", length * width);
-        System.out.printf(" Perimeter: %s", length * 2 + width * 2);
-        System.out.printf(" Volume is : %s", length * width * height);
+//        if(user4.equals(user1) || user4.equals(user2) || user4.equals(user3)){
+//            System.out.println("Sorry, that name has been taken");
+//        }else{
+//            System.out.println("Acceptable");
+//        }
+
+//        switch (user4){
+//            case "Christian":
+//            case "Marina":
+//            case "Miyavi":
+//                System.out.println("Sorry, that name has been taken");
+//                break;
+//            default:
+//                System.out.println("Acceptable");
+
+
+
+
+        }
+
 
     }
 
-}
+
