49dccc7cba25548f0d01a3c381725e881409e6e3
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-06-28 10:39:52 -0500

    adds first string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+        String firstString = "We don't need no education\nWe don't need no thought control";
+        String secondString = "Check \"this\" out! \"s inside of\"s!";
+        String thirdString = "In windows, the main drive is usually C:\\";
+        String fourthString = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+
+        System.out.println(firstString);
+        System.out.println(secondString);
+        System.out.println(thirdString);
+        System.out.println(fourthString);
+
+
+
+
+    }
+}
