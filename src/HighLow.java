df545d458c64bd9c8a142fbfc8997150f3371427
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-06-29 15:15:33 -0500

    adds highlow exercise

---
+++ src/HighLow.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class HighLow {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int random_int = (int) Math.floor(Math.random() * (100 + 1) + 0);
+        random_int += 1;
+
+        System.out.println("I wanna play a game...Please enter a number to begin.");
+        int userGuess = sc.nextInt();
+
+        do {
+            if (userGuess > random_int) {
+                System.out.println("LOWER");
+                System.out.println("Enter a new number");
+                userGuess = sc.nextInt();
+            } else if (userGuess < random_int) {
+                System.out.println("HIGHER");
+                System.out.println("Enter a new number");
+                userGuess = sc.nextInt();
+            }
+
+
+        } while (userGuess != random_int);
+        System.out.println("GOOD GUESS!");
+
+    }
+
+
+}
